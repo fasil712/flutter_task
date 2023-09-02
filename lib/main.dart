65ce1badd33582941e9ae0106b89c3e779b39cbc
Fasil Getie <fasilgetie12@gmail.com>
2023-09-02 11:38:43 +0300

    Update commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,63 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+// import 'package:flutter_authentication_flow_builder_bloc/firebase_options.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flow_builder/flow_builder.dart';
 
-void main() {
-  runApp(const MyApp());
+import '/repositories/repositories.dart';
+import '/bloc_observer.dart';
+import '/blocs/blocs.dart';
+import 'config/routes.dart';
+
+Future<void> main() {
+  return BlocOverrides.runZoned(
+    () async {
+      WidgetsFlutterBinding.ensureInitialized();
+      await Firebase.initializeApp(
+          // options: DefaultFirebaseOptions.currentPlatform,
+          );
+
+      final authRepository = AuthRepository();
+      runApp(App(authRepository: authRepository));
+    },
+    blocObserver: AppBlocObserver(),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class App extends StatelessWidget {
+  const App({
+    Key? key,
+    required AuthRepository authRepository,
+  })  : _authRepository = authRepository,
+        super(key: key);
+
+  final AuthRepository _authRepository;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Simple Flutter App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return RepositoryProvider.value(
+      value: _authRepository,
+      child: BlocProvider(
+        create: (_) => AppBloc(
+          authRepository: _authRepository,
+        ),
+        child: const AppView(),
       ),
-      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+class AppView extends StatelessWidget {
+  const AppView({
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Simple App Example'),
-      ),
-      body: const Center(
-        child: Text(
-          'Hello, Flutter!',
-          style: TextStyle(fontSize: 24),
-        ),
+    return MaterialApp(
+      home: FlowBuilder<AppStatus>(
+        state: context.select((AppBloc bloc) => bloc.state.status),
+        onGeneratePages: onGenerateAppViewPages,
       ),
     );
   }
