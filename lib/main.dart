8e4ddff45e79ee128fd4ddb6b9070db4a5bfca25
Fasil Getie <fasilgetie12@gmail.com>
2023-09-02 12:52:46 +0300

    update commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-// import 'package:flutter_authentication_flow_builder_bloc/firebase_options.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flow_builder/flow_builder.dart';
 
@@ -13,9 +12,7 @@ Future<void> main() {
   return BlocOverrides.runZoned(
     () async {
       WidgetsFlutterBinding.ensureInitialized();
-      await Firebase.initializeApp(
-          // options: DefaultFirebaseOptions.currentPlatform,
-          );
+      await Firebase.initializeApp();
 
       final authRepository = AuthRepository();
       runApp(App(authRepository: authRepository));
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,15 +2,46 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '/blocs/blocs.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   static Page page() => const MaterialPage<void>(child: HomeScreen());
 
   @override
-  Widget build(BuildContext context) {
-    final user = context.select((AppBloc bloc) => bloc.state.user);
+  State<HomeScreen> createState() => _HomeScreenState();
+}
 
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Search',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Booking',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: Notifications',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: Setting',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home'),
@@ -21,24 +52,69 @@ class HomeScreen extends StatelessWidget {
           )
         ],
       ),
-      body: Align(
-        alignment: const Alignment(0, 0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CircleAvatar(
-              radius: 48,
-              backgroundImage:
-                  user.photo != null ? NetworkImage(user.photo!) : null,
-              child: user.photo == null
-                  ? const Icon(Icons.person_outline, size: 48)
-                  : null,
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        iconSize: 24.0,
+        backgroundColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        // ignore: prefer_const_constructors
+        selectedItemColor: Color.fromARGB(255, 32, 126, 167),
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.search,
             ),
-            const SizedBox(height: 4),
-            Text(user.email ?? '', style: const TextStyle(fontSize: 24)),
-          ],
-        ),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.book,
+            ),
+            label: 'Booking',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.notifications,
+            ),
+            label: 'Notifications',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.settings,
+            ),
+            label: 'Setting',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
       ),
     );
   }
 }
+
+// class DefaultPage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     final user = context.select((AppBloc bloc) => bloc.state.user);
+//     return Align(
+//       alignment: const Alignment(0, 0),
+//       child: Column(
+//         mainAxisAlignment: MainAxisAlignment.center,
+//         children: [
+//           CircleAvatar(
+//             radius: 48,
+//             backgroundImage:
+//                 user.photo != null ? NetworkImage(user.photo!) : null,
+//             child: user.photo == null
+//                 ? const Icon(Icons.person_outline, size: 48)
+//                 : null,
+//           ),
+//           const SizedBox(height: 4),
+//           Text(user.email ?? '', style: const TextStyle(fontSize: 24)),
+//         ],
+//       ),
+//     );
+//   }
+// }
